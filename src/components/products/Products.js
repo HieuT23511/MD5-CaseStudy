488a402b1992750cc1a71ea9fd2a95135046ed3b
Hyboy95 <temilator@gmail.com>
2023-07-24 08:57:19 +0700

    update

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -16,9 +16,16 @@ export function Products() {
     const [products, setProducts] = useState(productDemo);
     return (
         <div className="py-10">
+            <div className="flex flex-col items-center gap-4">
+                <h1 className="text-2xl bg-black text-white py-2 w-80 text-center">shopping everyday</h1>
+                <span className="w-20 h-3 bg-black"></span>
+                <p className="max-w-[700px] text-gray-600 text-center">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p>
+            </div>
             <div className="max-w-screen-xl mx-auto grid grid-cols-4 gap-6">
                 {products.map((item, key) => {
-                    return <ProductCard product = {item} key={key}/>
+                    return <ProductCard product={item} key={key} />
                 })}
             </div>
         </div>
